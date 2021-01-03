e6c0454fe24a0d16156b3884772f9611ebafc3dc
jbopp <github@jbopp.de>
2021-01-03 02:08:44 +0100

    Version number update

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='codetempl',
-    version='0.3.0',
+    version='0.3.1',
     description='Code file generator',
     author='Fabian Meyer, Julian Bopp',
     author_email='user8324@posteo.net, julian.bopp@physik.hu-berlin.de',
